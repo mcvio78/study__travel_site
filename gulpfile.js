b87d0a5851e2e9421f42377bbe62fdffb7e08701
Mauro <mcvio78@gmail.com>
2018-02-06 22:39:32 +0100

    start with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
 var gulp = require('gulp');
 
-/*gulp.task('default', function() {
-	console.log("Hooray - you created a Gulp task!")
-});*/
+
+gulp.task('default', function() {
+	console.log("Hooray - you created a Gulp task!");
+});
+
+gulp.task('html', function() {
+	console.log('Immage something useful being done to your HTML here.');
+});
