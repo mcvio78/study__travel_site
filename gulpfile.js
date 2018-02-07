2bdb1d0afe4f56846e2e9b6b6370d9d0b6782e59
Mauro <mcvio78@gmail.com>
2018-02-07 13:43:12 +0100

    update files

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 var gulp = require('gulp');
-
+var watch = require('gulp-watch');
 
 gulp.task('default', function() {
 	console.log("Hooray - you created a Gulp task!");
@@ -7,4 +7,10 @@ gulp.task('default', function() {
 
 gulp.task('html', function() {
 	console.log('Immage something useful being done to your HTML here.');
+});
+
+gulp.task('watch', function() {
+	watch('./app/index.html', function(){
+		gulp.start('html');
+	});
 });
