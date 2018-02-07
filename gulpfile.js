488d660c3ac7d9f950d995c4073ab31a77acc0e8
Mauro <mcvio78@gmail.com>
2018-02-07 15:12:52 +0100

    new styles file

--- gulpfile.js
+++ gulpfile.js
@@ -9,8 +9,21 @@ gulp.task('html', function() {
 	console.log('Immage something useful being done to your HTML here.');
 });
 
+gulp.task('styles', function() {
+	console.log('Immage Sass or PostCSS tasks running here.');
+});
+
 gulp.task('watch', function() {
 	watch('./app/index.html', function(){
 		gulp.start('html');
 	});
+	
+	watch('./app/assets/styles/**/*.css', function() {
+		gulp.start('styles')
+	});
+	
 });
+
+
+
+
