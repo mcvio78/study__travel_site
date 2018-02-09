88a1e84018aa2e567dde964271901a53bee0792e
Mauro <mcvio78@gmail.com>
2018-02-09 20:29:37 +0100

    add files

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,9 @@ var gulp = require('gulp'),
 watch = require('gulp-watch'),
 postcss = require('gulp-postcss'),
 autoprefixer = require('autoprefixer'),
-cssvars = require ('postcss-simple-vars');
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested'),
+cssImport = require('postcss-import');
 
 gulp.task('default', function() {
 	console.log("Hooray - you created a Gulp task!");
@@ -14,7 +16,7 @@ gulp.task('html', function() {
 
 gulp.task('styles', function() {
 	return gulp.src('./app/assets/styles/styles.css')
-		.pipe(postcss([cssvars, autoprefixer]))
+		.pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
 		.pipe(gulp.dest('./app/temp/styles'));
 });
 
