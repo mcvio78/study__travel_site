b71c27b4e72b69ea9f13cadfd6192b6c6be1db26
Mauro <mcvio78@gmail.com>
2018-05-04 11:01:22 +0200

    Update 2018-05-04T09:00:45.561Z

--- gulpfile.js
+++
@@ -1,7 +0,0 @@
-require('./gulp/tasks/styles');
-require('./gulp/tasks/watch');
-require('./gulp/tasks/sprites');
-require('./gulp/tasks/scripts');
-require('./gulp/tasks/modernizr');
-require('./gulp/tasks/build');
-require('./gulp/tasks/gulp-gh-pages');
